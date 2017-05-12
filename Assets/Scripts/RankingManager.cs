23e1e2e76f6b5404c70c841af3d6df70129181e4
rarudo <rarudonet@gmail.com>
2017-05-12 16:20:55 +0900

    ランキング機能を構造体と、リストで実装

---
+++ Assets/Scripts/RankingManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RankingManager : MonoBehaviour
+{
+	[SerializeField]
+	private List<rank> rankList;
+
+    [Serializable]
+	public struct rank{
+		public int score;
+        public string name;
+	}
+
+	[Serializable]
+	public struct rankingStruct
+	{
+		public rank[] ranks;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		rank[] rankings;
+		rankings = new rank[10];
+		for (int i = 0; i < rankings.Length; i++)
+		{
+			rankings[i].score = i;
+			rankings[i].name = "taro" + i;
+		}
+		rankingStruct rans = new rankingStruct();
+		rans.ranks = rankings;
+
+
+		string json = JsonUtility.ToJson(rans);
+		print(json);
+		rankList = new List<rank>();
+		foreach (rank rank in rankings)
+		{
+			rankList.Add(rank);
+		}
+		string json2 = JsonUtility.ToJson(rankList);
+		print(json2);
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
