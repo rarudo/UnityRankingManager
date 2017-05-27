0af0009ace217469ac5d07a859f5dd54d43ca66f
rarudo <rarudonet@gmail.com>
2017-05-28 00:42:37 +0900

    Script ランキング機能の実装

--- Assets/Scripts/RankingManager.cs
+++ Assets/Scripts/RankingManager.cs
@@ -1,56 +1,65 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 
 public class RankingManager : MonoBehaviour
 {
-	[SerializeField]
-	private List<rank> rankList;
-
-    [Serializable]
-	public struct rank{
-		public int score;
-        public string name;
-	}
-
-	[Serializable]
-	public struct rankingStruct
-	{
-		public rank[] ranks;
-	}
-
+    FileInfo fi;
+	List<int> scores = new List<int>();
+	
 	// Use this for initialization
 	void Start ()
 	{
-		rank[] rankings;
-		rankings = new rank[10];
-		for (int i = 0; i < rankings.Length; i++)
-		{
-			rankings[i].score = i;
-			rankings[i].name = "taro" + i;
-		}
-		rankingStruct rans = new rankingStruct();
-		rans.ranks = rankings;
-
-
-		string json = JsonUtility.ToJson(rans);
-		print(json);
-		rankList = new List<rank>();
-		foreach (rank rank in rankings)
-		{
-			rankList.Add(rank);
-		}
-		string json2 = JsonUtility.ToJson(rankList);
-		print(json2);
-
-
-
+        fi = new FileInfo("./Assets/Saves/Score.csv");
+		loadSave();
 	}
+	
+	public void insertScore(int score)
+	{
+		scores.Add(score);
+        StreamWriter sw;
+		sw = fi.AppendText();
+		sw.WriteLine(score);
+		sw.Flush();
+		sw.Close();
+	}
+
+	private void loadSave()
+	{
+        // FileReadTest.txtファイルを読み込む
+        try {
+            using (StreamReader sr = new StreamReader(fi.OpenRead(), Encoding.UTF8)){
+                // 一行毎読み込み
+	            while(sr.Peek() >= 0){
+                    string readLine = sr.ReadLine();
+                    scores.Add(int.Parse(readLine));
+                }
+            }
+        } catch (Exception e){
+            // 改行コード
+	        print(e);
+        }
+	}
+
+	public List<int> getSortedScores()
+	{
+		scores.Sort();
+		scores.Reverse();
+		return scores;
+	}
+
+	public int getHighScore()
+	{
+		return getSortedScores()[0];
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
